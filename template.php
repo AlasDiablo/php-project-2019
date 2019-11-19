96bb42fc836a29a0f942d237d2d3e11def585119
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2019-11-19 15:40:31 +0100

    Ajout d'un template pour la creation des page html + php

---
+++ template.php
@@ -0,0 +1,14 @@
+<?php
+ // code php ici
+?>
+<!-- code html ici  -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
