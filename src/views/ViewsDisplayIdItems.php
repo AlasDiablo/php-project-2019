b31a8dd474094667f2abc510fad4f23a23964cc9
lucasburte <lucas.burte@hotmail.fr>
2019-11-26 15:23:52 +0100

    modification vue idItems

--- src/views/ViewsDisplayIdItems.php
+++ src/views/ViewsDisplayIdItems.php
@@ -5,4 +5,18 @@ namespace mywishlist\views;
 class ViewsDisplayIdItems
 {
 
+    public static function getIdItems($item_by_id) {
+        foreach ($item_by_id as $key => $value) {
+            print $key . ': <br>';
+            print '  id: ' . $value['id'] . '<br>' .
+                ' liste_id: ' . $value['liste_id'] . '<br>' .
+                ' nom: ' . $value['nom'] . '<br>' .
+                ' descr: ' . $value['descr'] . '<br>' .
+                ' img: ' . $value['img'] . '<br>' .
+                ' url: ' . $value['url'] . '<br>' .
+                ' tarif: ' . $value['tarif'] . '<br>';
+            print  '<br>';
+        }
+    }
+
 }
