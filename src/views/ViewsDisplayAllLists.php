0d2ddb21661b08f5b64599f27853b80e3fc4a4a3
Aurel_11 <55143780+Aurel-11@users.noreply.github.com>
2019-11-26 15:22:14 +0100

    Réalisation de la vue AllLists

--- src/views/ViewsDisplayAllLists.php
+++ src/views/ViewsDisplayAllLists.php
@@ -4,5 +4,15 @@ namespace mywishlist\views;
 
 class ViewsDisplayAllLists
 {
-
+    static function displayAllLists(){
+        foreach ($list as $key => $value) {
+            print $key . ': <br>';
+            print '  no: ' . $value['no'] . '<br>' .
+                ' user_id: ' . $value['titre'] . '<br>' .
+                ' description: ' . $value['description'] . '<br>' .
+                ' expiration: ' . $value['expiration'] . '<br>' .
+                ' token: ' . $value['token'] . '<br>';
+            print  '<br>';
+        }
+    }
 }
