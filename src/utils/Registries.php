af156b5f10e278b56c8ee88b5943355cd6e011eb
Safyrus <54881649+Safyrus@users.noreply.github.com>
2019-12-04 15:37:05 +0100

    modif registries

--- src/utils/Registries.php
+++ src/utils/Registries.php
@@ -4,17 +4,17 @@ namespace mywishlist\utils;
 
 class Registries
 {
-    // chemien et nom de la rasine
+    // chemin et nom de la racine
     const ROOT = 'ROOT';
     const ROOT_PATH = '/';
 
-    // chemien et nom de l'incription
+    // chemin et nom de l'inscription
     const REGISTER = 'REGISTER';
     const REGISTER_POST = 'REGISTER_POST';
     const REGISTER_PATH = '/user/register';
     const REGISTER_POST_PATH = '/user/register/post';
 
-    // chemien et nom de login
+    // chemin et nom de login
     const LOGIN = 'LOGIN';
     const LOGIN_PATH = '/user/login';
 
