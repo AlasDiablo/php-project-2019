f668f8d9b38c63977427696ef8ea0eddddbe549a
Aurel_11 <55143780+Aurel-11@users.noreply.github.com>
2019-12-09 11:14:50 +0100

    Retour à zéro de la gestion des listes et des items

--- src/views/ItemView.php
+++ src/views/ItemView.php
@@ -11,39 +11,6 @@ class ItemView implements IView
 
     public function render($code, $data_set): array
     {
-        if ($code == Registries::ITEMALL) {
-            return array(
-                'css' => '',
-                'html' => $this->displayAllitems($data_set)
-            );
-        }
-    }
 
-    private function displayAllitems($list){
-        $str = '';
-        foreach ($list as $key => $value) {
-            $str .= $key . ': <br>';
-            $str .= '  no: ' . $value['no'] . '<br>' .
-                ' user_id: ' . $value['titre'] . '<br>' .
-                ' description: ' . $value['description'] . '<br>' .
-                ' expiration: ' . $value['expiration'] . '<br>' .
-                ' token: ' . $value['token'] . '<br>';
-            $str .=  '<br>';
-        }
-        return $str;
-    }
-
-    private function getIdItems($item_by_id) {
-        foreach ($item_by_id as $key => $value) {
-            print $key . ': <br>';
-            print '  id: ' . $value['id'] . '<br>' .
-                ' liste_id: ' . $value['liste_id'] . '<br>' .
-                ' nom: ' . $value['nom'] . '<br>' .
-                ' descr: ' . $value['descr'] . '<br>' .
-                ' img: ' . $value['img'] . '<br>' .
-                ' url: ' . $value['url'] . '<br>' .
-                ' tarif: ' . $value['tarif'] . '<br>';
-            print  '<br>';
-        }
     }
 }
--- src/views/ListView.php
+++ src/views/ListView.php
@@ -9,23 +9,8 @@ class ListView implements IView
 
     public function render($code, $data_set): array
     {
-        return array(
-            'css' => '',
-            'html' => $this->displayAllLists($data_set)
-        );
+
     }
 
-    private function displayAllLists($list){
-        $str = "";
-        foreach ($list as $key => $value) {
-            $str .= $key . ': <br>';
-            $str .= '  no: ' . $value['no'] . '<br>' .
-                ' user_id: ' . $value['titre'] . '<br>' .
-                ' description: ' . $value['description'] . '<br>' .
-                ' expiration: ' . $value['expiration'] . '<br>' .
-                ' token: ' . $value['token'] . '<br>';
-            $str .=  '<br>';
-        }
-        return $str;
-    }
+
 }
