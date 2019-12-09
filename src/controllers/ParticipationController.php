4a92f5ab8409cae04f9182609a03a16ce7a89ee7
Safyrus <54881649+Safyrus@users.noreply.github.com>
2019-12-09 10:50:12 +0100

    modif Participation

---
+++ src/controllers/ParticipationController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace mywishlist\controllers;
+
+
+use mywishlist\models\Liste;
+use mywishlist\models\Item;
+use mywishlist\views\RenderHandler;
+use mywishlist\utils\Registries;
+
+class Participation
+{
+    public static function displayAllLists(){
+
+        $list = Liste::all();
+        $r = new RenderHandler(Registries::LISTALL, $list);
+        $r->render();
+    }
+
+    public static function displayAllItems()
+    {
+        $item = Item::all();
+        $r = new RenderHandler(Registries::ITEMALL, $item);
+        $r->render();
+    }
+}
--- src/views/ParticipationView.php
+++ src/views/ParticipationView.php
@@ -3,12 +3,59 @@
 
 namespace mywishlist\views;
 
+use mywishlist\utils\Registries;
 
 class ParticipationView implements IView
 {
-
     public function render($code, $data_set): array
     {
-        // TODO: Implement render() method.
+        switch ($code) {
+            case Registries::LISTALL:
+                return array(
+                    'css' => '',
+                    'html' => $this->displayAllLists($data_set)
+                );
+                break;
+            case Registries::ITEMALL:
+                return array(
+                    'css' => '',
+                    'html' => $this->displayAllItems($data_set)
+                );
+                break;
+            default:
+                return array(
+                    'css' => '',
+                    'html' => "<p>404</p>"
+                );
+                break;
+        }
+    }
+
+    private function displayAllLists($list){
+        $str = "";
+        foreach ($list as $key => $value) {
+            $str .= $key . ': <br>';
+            $str .= '  no: ' . $value['no'] . '<br>' .
+                ' user_id: ' . $value['titre'] . '<br>' .
+                ' description: ' . $value['description'] . '<br>' .
+                ' expiration: ' . $value['expiration'] . '<br>' .
+                ' token: ' . $value['token'] . '<br>';
+            $str .=  '<br>';
+        }
+        return $str;
+    }
+
+    private function displayAllitems($list){
+        $str = '';
+        foreach ($list as $key => $value) {
+            $str .= $key . ': <br>';
+            $str .= '  no: ' . $value['no'] . '<br>' .
+                ' user_id: ' . $value['titre'] . '<br>' .
+                ' description: ' . $value['description'] . '<br>' .
+                ' expiration: ' . $value['expiration'] . '<br>' .
+                ' token: ' . $value['token'] . '<br>';
+            $str .=  '<br>';
+        }
+        return $str;
     }
 }
