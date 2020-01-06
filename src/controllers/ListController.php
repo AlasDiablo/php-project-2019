41dc1515cc7f691eec989c3dfdb31aaec8de04eb
Safyrus <54881649+Safyrus@users.noreply.github.com>
2020-01-06 15:18:04 +0100

    modif affichage list

--- src/controllers/ListController.php
+++ src/controllers/ListController.php
@@ -79,6 +79,8 @@ class ListController {
             'items' => $items,
             'authors' => $authors,
             'title' => Liste::select('titre')->where('no', '=', $id)->first()->titre,
+            'desc' => Liste::select('description')->where('no', '=', $id)->first()->description,
+            'exp' => Liste::select('expiration')->where('no', '=', $id)->first()->expiration,
             'id' => $id_list
         );
 
