02373d9e92d7b11e8a1148f02005d2fe391c6da5
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2020-01-15 21:23:26 +0100

    Ajout de doc et pofinage de l'aparance du code dans `Authentication.php` et `Selection`.

--- src/utils/Authentication.php
+++ src/utils/Authentication.php
@@ -1,22 +1,33 @@
 <?php
 
-
 namespace mywishlist\utils;
 
 use mywishlist\models\User;
 
+/**
+ * Class Authentication, Class appelé pour tous se qui conserne l'utilisateurs
+ * @package mywishlist\utils
+ */
 class Authentication
 {
+    /**
+     * Tous les constante corespondant au niveau d'action des utilisateurs
+     */
     const ANONYMOUS  = 0; // default user id & user level
     const USER = 1; // user level by default
     const ADMIN = 2;
     const SUPER_ADMIN = 3;
 
+    /**
+     * Fonction qui permette d'optenir le niveau d'action du l'utilisé
+     * @return int niveau d'action
+     */
     public static function getUserLevel(): int
     {
         if (isset($_SESSION['user_id']))
         {
-            if (isset(User::select('user_level')->where('user_id', '=', $_SESSION['user_id'])->get()[0])) {
+            if (isset(User::select('user_level')->where('user_id', '=', $_SESSION['user_id'])->get()[0]))
+            {
                 return User::select('user_level')->where('user_id', '=', $_SESSION['user_id'])->get()[0]['user_level'];
             } else {
                 $_SESSION['user_id'] = self::ANONYMOUS;
@@ -28,6 +39,10 @@ class Authentication
         }
     }
 
+    /**
+     * Fonction qui permette d'avoir le nom d'utilisateurs associer a l'utilsateurs
+     * @return string nom d'utilisateurs associer a l'utilsateurs
+     */
     public static function getUsername()
     {
         if (self::getUserLevel() != self::ANONYMOUS)
@@ -38,6 +53,10 @@ class Authentication
         }
     }
 
+    /**
+     * Fonction qui permette d'avoir l'id associée a l'utilisateur
+     * @return int|mixed id de l'utisateurs
+     */
     public static function getUserId()
     {
         if (self::getUserLevel() != self::ANONYMOUS)
@@ -48,6 +67,10 @@ class Authentication
         }
     }
 
+    /**
+     * Fonction utilisé pour optenir l'emil de l'utisateurs
+     * @return string email de l'utilisateurs
+     */
     public static function getEmail()
     {
         if (self::getUserLevel() != self::ANONYMOUS)
--- src/utils/Selection.php
+++ src/utils/Selection.php
@@ -3,34 +3,30 @@
 
 namespace mywishlist\utils;
 
-
+/**
+ * Class Selection, cette class permette de stocké tous les contente associer au rendu et autre
+ * @package mywishlist\utils
+ */
 class Selection
 {
     const ALL_LIST = 'ALL_LIST';
-    const ID_LIST = 'ID_LIST';
     const TOKEN_LIST_MODIFIABLE = 'TOKEN_LIST_MODIFIABLE';
     const TOKEN_LIST = 'TOKEN_LIST';
     const FORM_CREATE_LIST = 'FORM_CREATE_LIST';
     const FORM_MODIFY_LIST = 'FORM_MODIFY_LIST';
-    const SHARE_LIST = 'SHARE_LIST';
 
-    const ALL_ITEM = 'ALL_ITEM';
     const ID_ITEM = 'ID_ITEM';
     const FORM_CREATE_ITEM = 'FORM_CREATE_ITEM';
-    const FORM_ITEM_RESERVE = 'FORM_ITEM_RESERVE';
     const FORM_ITEM_RESERVE_FAIL = 'FORM_ITEM_RESERVE_FAIL';
     const FORM_ITEM_RESERVE_SUCCESS = 'FORM_ITEM_RESERVE_SUCCESS';
     const FORM_IMAGE_UPLOAD_FAIL = 'FORM_IMAGE_UPLOAD_FAIL';
     const FORM_IMAGE_UPLOAD_SUCCESS = 'FORM_IMAGE_UPLOAD_SUCCESS';
 
     const ACCOUNT = 'ACCOUNT';
-    const REGISTER_POST = 'REGISTER_POST';
     const REGISTER_POST_SUCCESS = 'REGISTER_POST_SUCCESS';
     const REGISTER_POST_FAILED = 'REGISTER_POST_FAILED';
     const REGISTER_POST_USER_OR_EMAIL_EXSITE = 'REGISTER_POST_USER_OR_EMAIL_EXSITE';
-    const DATA_FAILED = 'DATA_FAILED';
 
-    const LOGIN_POST = 'LOGIN_POST';
     const LOGIN_POST_SUCCESS = 'LOGIN_SUCCESS';
     const LOGIN_POST_FAILED = 'LOGIN_POST_FAILED';
     const LOGIN_POST_USERPASS_WRONG = "LOGIN_POST_USERPASS_WRONG";
